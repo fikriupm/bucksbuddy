608274674eff1eeb145f3b560f98ae5f5f80674f
FikriZaid <88956418+FikriZaid@users.noreply.github.com>
2024-06-08 12:16:20 +0800

    profile picture updaated

--- lib/common/widgets/images/t_circular_image.dart
+++ lib/common/widgets/images/t_circular_image.dart
@@ -1,3 +1,5 @@
+import 'package:bucks_buddy/utils/shimmer/shimmer.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:bucks_buddy/utils/constants/colors.dart';
 import 'package:bucks_buddy/utils/constants/sizes.dart';
@@ -6,14 +8,14 @@ import 'package:bucks_buddy/utils/helpers/helper_functions.dart';
 class TCircularImage extends StatelessWidget {
   const TCircularImage({
     super.key,
-    this.fit, 
-    required this.image, 
-    this.isNetworkImage = false, 
-    this.overLayColor, 
-    this.backgroundColor, 
-    this.width = 56, 
-    this.height = 56, 
-    this.padding = TSizes.sm, 
+    this.fit,
+    required this.image,
+    this.isNetworkImage = false,
+    this.overLayColor,
+    this.backgroundColor,
+    this.width = 56,
+    this.height = 56,
+    this.padding = TSizes.sm,
   });
 
   final BoxFit? fit;
@@ -30,17 +32,31 @@ class TCircularImage extends StatelessWidget {
       height: height,
       padding: EdgeInsets.all(padding),
       decoration: BoxDecoration(
-        color: backgroundColor ?? (THelperFunctions.isDarkMode(context) ? TColors.black : TColors.white),
+        color: backgroundColor ??
+            (THelperFunctions.isDarkMode(context)
+                ? TColors.black
+                : TColors.white),
         borderRadius: BorderRadius.circular(100),
       ),
-      child: Center(
-        child: Image(
-          fit: fit,
-          image: isNetworkImage ? NetworkImage(image) : AssetImage(image) as ImageProvider,
-          color: overLayColor,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(100),
+        child: Center(
+          child: isNetworkImage
+              ? CachedNetworkImage(
+                  fit: fit,
+                  color: overLayColor,
+                  imageUrl: image,
+                  progressIndicatorBuilder: (context, url, downloadProgrss) =>
+                      const TShimmerEffect(width: 55, height: 55, radius: 55,),
+                  errorWidget: (context, url, error) => const Icon(Icons.error),
+                )
+              : Image(
+                  fit: fit,
+                  image: AssetImage(image),
+                  color: overLayColor,
+                ),
         ),
       ),
-    
     );
   }
 }
--- lib/features/home/screens/widget/user_section.dart
+++ lib/features/home/screens/widget/user_section.dart
@@ -1,3 +1,4 @@
+import 'package:bucks_buddy/common/widgets/images/t_circular_image.dart';
 import 'package:bucks_buddy/features/personalization/controllers/user_controller.dart';
 import 'package:bucks_buddy/utils/constants/image_strings.dart';
 import 'package:bucks_buddy/utils/constants/sizes.dart';
@@ -16,36 +17,60 @@ class UserSection extends StatelessWidget {
     final controller = Get.put(UserController());
     return Row(
       children: [
-        const Image(image: AssetImage(TImages.user)),
-        Column(
-          children: [
-            const Text(TTexts.profile),
-            Row(
-              children: [
-                Obx(() {
-                  if (controller.profileLoading.value) {
-                    //display a shimmer loading while user profile is being laoded
-                    return const TShimmerEffect(width: 80, height: 15);
-                  } else {
-                    return Text(controller.user.value.fullName);
-                  }
-                }),
-                const SizedBox(
-                  width: TSizes.defaultSpace,
-                ),
-                const Icon(
-                  Icons.waving_hand_rounded,
-                  size: TSizes.iconSm,
+        Obx(() {
+          final networkImage = controller.user.value.profilePicture;
+          final image = networkImage.isNotEmpty ? networkImage : TImages.user;
+          return controller.imageUpLoading.value
+              ? const TShimmerEffect(
+                  width: 80,
+                  height: 80,
+                  radius: 80,
                 )
-              ],
-            ),
-          ],
+              : TCircularImage(
+                  image: image,
+                  width: 80,
+                  height: 80,
+                  isNetworkImage: networkImage.isNotEmpty,
+                );
+        }),
+        const SizedBox(width: TSizes.defaultSpace),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(TTexts.profile),
+              Row(
+                children: [
+                  Obx(() {
+                    if (controller.profileLoading.value) {
+                      // Display a shimmer loading while user profile is being loaded
+                      return const TShimmerEffect(width: 80, height: 15);
+                    } else {
+                      return Expanded(
+                        child: Text(
+                          controller.user.value.fullName,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      );
+                    }
+                  }),
+                  const SizedBox(
+                    width: TSizes.defaultSpace / 10,
+                  ),
+                  const Icon(
+                    Icons.waving_hand_rounded,
+                    size: TSizes.iconSm,
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
         const Spacer(),
         const Padding(
-          padding: EdgeInsets.only(right: TSizes.defaultSpace),
+          padding: EdgeInsets.only(right: TSizes.defaultSpace / 2),
           child: Icon(Icons.notifications_none_sharp),
-        )
+        ),
       ],
     );
   }
